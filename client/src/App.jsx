a8dd0ab4f91d2118cd2d449d8f8b9c11700eb02d
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-11-11 15:53:16 -0600

    app.jsx in client/src directory should  be complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'; // eslint-disable-line no-unused-vars
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
